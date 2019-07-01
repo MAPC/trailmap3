a5b7fa8a30a49d998b44ea04ff420221b9be1211
Matthew Zagaja <mzagaja@alum.wpi.edu>
2019-07-01 15:51:38 -0400

    Prove Placement of MultiLineString GeoJSON Objects
    Initial spike to show placement of a MultiLineString in Mapbox

--- app/javascript/pages/components/map.jsx
+++ app/javascript/pages/components/map.jsx
@@ -9,11 +9,20 @@ const data = fromJS({
     version: 8,
     sources: {
         points: {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: [
+              {type: 'Feature', geometry: {type: 'Point', coordinates: [-71.0589, 42.3601]}}
+            ]
+          }
+        },
+        trailmap: {
             type: 'geojson',
             data: {
                 type: 'FeatureCollection',
                 features: [
-                    {type: 'Feature', geometry: {type: 'Point', coordinates: [-71.0589, 42.3601]}}
+                    {type: 'Feature', geometry: {type: 'MultiLineString', coordinates: [[[-70.9985, 42.4184], [-70.9981, 42.4184]]]}}
                 ]
             }
         }
@@ -35,9 +44,13 @@ const layers = fromJS({
         id: 'trails',
         type: 'line',
         source: 'trailmap',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
         paint: {
-          'line-color': '#877b59',
-          'line-width': 1
+          'line-color': '#f00',
+          'line-width': 4
         }
       }
   ]
@@ -46,9 +59,11 @@ const layers = fromJS({
 const defaultMapStyle = fromJS(MAP_STYLE);
 const mapStyleWithData = defaultMapStyle
   // Add geojson source to map
-  .setIn(['sources', 'trailmap'], data.getIn(['sources', 'points']))
+  .setIn(['sources', 'trailmap'], data.getIn(['sources', 'trailmap']))
   // Add point layer to map
   .set('layers', defaultMapStyle.get('layers').push(layers.get('layers').get(1)));
+
+// debugger;
 
 // fetch my GeoJSON
 // curl "https://mapc-admin.carto.com/api/v2/sql?q=SELECT%20ST_AsGeoJSON(the_geom)%20as%20the_geom%20FROM%20bike_facilities%20LIMIT%2030"
@@ -70,7 +85,8 @@ export default class Map extends Component {
   componentDidMount() {
     requestJson('https://mapc-admin.carto.com/api/v2/sql?q=SELECT%20ST_AsGeoJSON(the_geom)%20as%20the_geom%20FROM%20bike_facilities%20LIMIT%2030', (error, response) => {
       if (!error) {
-        // JSON.parse(response.rows[0].the_geom)
+
+        // let parsed_response = JSON.parse(response)
         // debugger;
       }
     });
