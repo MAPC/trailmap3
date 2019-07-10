d42219d64e6d3c15463daf428180d5d21e377ed2
Matthew Zagaja <mzagaja@alum.wpi.edu>
2019-07-10 11:32:53 -0400

    Spike Working Layer Feature
    * Use updated d3-fetch to work with Promise.all
    * Add yarn install to setup for easier setup for new users
    * Add walking and bicycle layers to map with toggles

--- app/javascript/pages/components/map.jsx
+++ app/javascript/pages/components/map.jsx
@@ -1,31 +1,16 @@
 import React from 'react';
 import {Component} from 'react';
 import ReactMapGL from 'react-map-gl';
-import {json as requestJson} from 'd3-request';
+import {json as requestJson} from 'd3-fetch';
 import {fromJS} from 'immutable';
 import MAP_STYLE from './map-style-basic-v8.json';
 import '../../styles/map'
 
-const data = fromJS({
-    version: 8,
-    sources: {
-        trailmap: {
-            type: 'geojson',
-            data: {
-                type: 'FeatureCollection',
-                features: [
-                    {type: 'Feature', geometry: {type: 'MultiLineString', coordinates: [[[-70.9985, 42.4184], [-70.9981, 42.4184]]]}}
-                ]
-            }
-        }
-    }
-});
-
 const layers = fromJS({
   layers: [{
-        id: 'trails',
+        id: 'Bicycle Trails',
         type: 'line',
-        source: 'trailmap',
+        source: 'bike_facilities',
         layout: {
           'line-join': 'round',
           'line-cap': 'round',
@@ -35,30 +20,36 @@ const layers = fromJS({
           'line-color': '#f00',
           'line-width': 4,
         }
+      },
+      {
+        id: 'Walking Trails',
+        type: 'line',
+        source: 'walking_trails',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round',
+          'visibility': 'visible'
+        },
+        paint: {
+          'line-color': '#0000ff',
+          'line-width': 4,
+        }
       }]
 })
 
 const defaultMapStyle = fromJS(MAP_STYLE);
-const mapStyleWithData = defaultMapStyle
-  // Add geojson source to map
-  .setIn(['sources', 'trailmap'], data.getIn(['sources', 'trailmap']))
-  // Add point layer to map
-  .set('layers', defaultMapStyle.get('layers').push(layers.get('layers').find(layer => layer.get('id') === 'trails')));
 
 export default class Map extends Component {
 
   state = {
-    mapStyle: mapStyleWithData,
+    mapStyle: defaultMapStyle,
     viewport: {
       width: "100vw",
       height: "100vh",
       latitude: 42.3601,
       longitude: -71.0589,
       zoom: 10
-    },
-    visibility: {
-      trails: true,
-    },
+    }
   };
 
   isVisible(visibility) {
@@ -88,20 +79,32 @@ export default class Map extends Component {
     })
   }
 
+  addLayer(newData, source) {
+    const geoJson = newData.rows.map(rows => ({type: 'Feature', geometry: JSON.parse(rows.the_geom)}));
+    const mapStyleWithGeoJsonType = this.state.mapStyle.setIn(['sources', source], { type: 'geojson' })
+    const mapStyleWithDataType = mapStyleWithGeoJsonType.setIn(['sources', source, 'data'], { type: 'FeatureCollection' })
+    const mapStyleWithNewSource = mapStyleWithDataType.setIn(['sources', source, 'data', 'features'], geoJson)
+    const mapStyleWithNewLayer = mapStyleWithNewSource.set('layers', this.state.mapStyle.get('layers').push(layers.get('layers').find(layer => layer.get('source') === source)))
+
+    this.setState({
+      mapStyle: mapStyleWithNewLayer
+    })
+  }
+
   componentDidMount() {
-    requestJson('https://mapc-admin.carto.com/api/v2/sql?q=SELECT%20ST_AsGeoJSON(the_geom)%20as%20the_geom%20FROM%20bike_facilities', (error, response) => {
-      if (!error) {
-
-        const trails = response.rows.map(rows => ({type: 'Feature', geometry: JSON.parse(rows.the_geom)}));
-        const trailLayer = this.state.mapStyle.get('layers').filter(layer => layer.get('id') === 'trails');
-        const mapStyleWithTrails = mapStyleWithData.setIn(['sources', 'trailmap', 'data', 'features'], trails)
-        // const trailLayerIndex = this.state.mapStyle.get('layers').findIndex(layer => layer.get('id') === 'trails')
-        // const mapStyleWithInvisibleTrails = mapStyleWithTrails.setIn(['layers', trailLayerIndex, 'layout', 'visibility'], 'none')
-
-        this.setState({
-          mapStyle: mapStyleWithTrails
-        })
-      }
+    Promise.all([
+        requestJson('https://mapc-admin.carto.com/api/v2/sql?q=SELECT%20fac_type,fac_stat,ST_AsGeoJSON(the_geom)%20as%20the_geom%20FROM%20bike_facilities'),
+        requestJson('https://mapc-admin.carto.com/api/v2/sql?q=SELECT%20fac_type,fac_stat,ST_AsGeoJSON(the_geom)%20as%20the_geom%20FROM%20walking_trails'),
+        requestJson('https://mapc-admin.carto.com/api/v2/sql?q=SELECT%20fac_stat,ST_AsGeoJSON(the_geom)%20as%20the_geom%20FROM%20landline_regional_greenways')
+      ]).then((map) => {
+        this.addLayer(map[0], 'bike_facilities');
+        this.addLayer(map[1], 'walking_trails');
+        // SELECT * FROM bike_facilities WHERE fac_type=2 AND fac_stat IN (2,3)
+        // SELECT * FROM bike_facilities WHERE fac_type=9 AND fac_stat IN (2,3)
+        // SELECT * FROM bike_facilities WHERE fac_type=1 AND fac_stat IN (2,3)
+        // SELECT * FROM walking_trails WHERE fac_stat IN (2,3)
+        // SELECT * FROM bike_facilities WHERE fac_type=5 AND fac_stat IN (2,3)
+        // select * from landline_regional_greenways WHERE fac_stat IN (2,3)
     });
   }
 
@@ -122,8 +125,6 @@ export default class Map extends Component {
 
 
   render() {
-    const displayedLayers = this.state.mapStyle.get('layers')
-
     return (
       <div className="test">
         <ReactMapGL
@@ -133,7 +134,7 @@ export default class Map extends Component {
           mapStyle={this.state.mapStyle}
         />
         <div className="control-panel">
-          { layers.get('layers').map(layer => this.renderLayerControl(layer.get('id'))) }
+          { this.state.mapStyle.get('layers').map(layer => this.renderLayerControl(layer.get('id'))) }
         </div>
        </div>
     );
