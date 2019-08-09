a7fbf49f98ed1dfc97dfbf25018f1a7ace283be8
Matthew Zagaja <mzagaja@alum.wpi.edu>
2019-08-09 11:36:37 -0400

    Copy and Deploy master.key
    We needed a secret key base so we deploy using master.key.

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :repo_url, "git@github.com:mapc/trailmap3.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, "config/master.key"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
