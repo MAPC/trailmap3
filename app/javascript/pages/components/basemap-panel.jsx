479b6d25067d308e2f686ed3d01ee6da1c2898b6
Annabelle Thomas Taylor <ataylor@mapc.org>
2019-09-24 11:11:06 -0400

    Resolves #15: Add basemap layers
    - Apply linter edits to panel and map
    - Set base maps to Cali Terrain and MapBox Lite
    - Copy over path_overlay and proposed_overlay trails on changeBasemap
    - Add basemap panel in addition ot filter panel
    - Add basemap thumbnail images
    - Alter styles for mobile responsiveness

---
+++ app/javascript/pages/components/basemap-panel.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { fromJS } from 'immutable';
+import MAP_STYLE from './light.json';
+import SECOND_MAP_STYLE from './cali.json';
+
+const mapboxLite = fromJS(MAP_STYLE);
+const caliTerrain = fromJS(SECOND_MAP_STYLE);
+
+export default class BasemapPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        basemap: mapboxLite,
+    };
+    this.el = document.createElement('div');
+    this.hideFilters = this.hideFilters.bind(this);
+  }
+
+  hideFilters() {
+    const basemapPanel = document.getElementsByClassName('basemap-panel')[0];
+    basemapPanel.className = 'basemap-panel basemap-panel--hidden';
+  }
+
+  render() {
+    return (
+      <div id="basemap-panel" className="basemap-panel--hidden">
+        <h2 className="basemap-panel__title">Basemaps</h2>
+        <button
+          className="basemap-panel__close"
+          type="button"
+          onClick={this.hideFilters}
+        >
+          X
+        </button>
+        <div className="basemap-panel__options">
+          <button
+            onClick={this.props.changeBasemap.bind(this, mapboxLite)}
+            type="button"
+            className="basemap-panel__options-lite"
+          />
+          <button
+            onClick={this.props.changeBasemap.bind(this, caliTerrain)}
+            type="button"
+            className="basemap-panel__options-cali"
+          />
+
+        </div>
+      </div>
+    );
+  }
+}
--- app/javascript/pages/components/map.jsx
+++ app/javascript/pages/components/map.jsx
@@ -1,98 +1,121 @@
-import React from 'react';
-import {Component} from 'react';
-import ReactMapGL, {NavigationControl, GeolocateControl} from 'react-map-gl';
-import {json as requestJson} from 'd3-fetch';
+import React, { Component } from 'react';
+import ReactMapGL, { NavigationControl, GeolocateControl } from 'react-map-gl';
 import mapboxgl from 'mapbox-gl';
-import {fromJS} from 'immutable';
-import MAP_STYLE from './map-style-basic-v8.json';
-import '../../styles/map'
-import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
-import Geocoder from 'react-map-gl-geocoder'
-import ControlPanel from './control-panel'
-import ControlPanelToggleButton from './control-panel-toggle-button'
-import AboutButton from './about-button'
-import AboutPanel from './about-panel'
+import { fromJS } from 'immutable';
+import '../../styles/map.scss';
+import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import Geocoder from 'react-map-gl-geocoder';
+import ControlPanel from './control-panel';
+import ControlPanelToggleButton from './control-panel-toggle-button';
+import AboutButton from './about-button';
+import AboutPanel from './about-panel';
+import BasemapButton from './basemap-button';
+import BasemapPanel from './basemap-panel'
+import MAP_STYLE from './light.json';
 
 const defaultMapStyle = fromJS(MAP_STYLE);
 
 export default class Map extends Component {
-
   constructor(props) {
     super(props);
+    this.state = {
+      mapStyle: defaultMapStyle,
+      viewport: {
+        latitude: 42.3601,
+        longitude: -71.0589,
+        zoom: 10,
+      },
+    };
     this.mapRef = React.createRef();
     this.updateStateWith = this.updateStateWith.bind(this);
+    this.changeBasemap = this.changeBasemap.bind(this);
   }
 
-  state = {
-    mapStyle: defaultMapStyle,
-    viewport: {
-      latitude: 42.3601,
-      longitude: -71.0589,
-      zoom: 10
-    },
-    map: ''
-  };
+  componentDidMount() {
+    const mapboxobj = this.mapRef.current.getMap();
+    mapboxobj.addControl(new mapboxgl.ScaleControl({
+      maxWidth: 100,
+      unit: 'imperial',
+    }), 'bottom-right');
+  }
 
   updateStateWith(updatedMapStyle) {
-    this.setState({
-        mapStyle: updatedMapStyle,
+    this.setState({ mapStyle: updatedMapStyle });
+  }
+
+  changeBasemap(updatedMapStyle) {
+    this.setState((prevState) => {
+      const trailSource = prevState.mapStyle.get('sources').get('path_overlay');
+      const trailLayers = prevState.mapStyle.get('layers').find(layer => layer.get('source') === 'path_overlay');
+
+      const proposedTrailSource = prevState.mapStyle.get('sources').get('proposed_overlay');
+      const proposedTrailLayers = prevState.mapStyle.get('layers').find(layer => layer.get('source') === 'proposed_overlay');
+      let newMapStyle = updatedMapStyle;
+      
+      if (trailLayers !== undefined) {
+        newMapStyle = updatedMapStyle
+          .setIn(['sources', 'path_overlay'], { type: 'geojson' })
+          .setIn(['sources', 'path_overlay', 'data'], { type: 'FeatureCollection' })
+          .setIn(['sources', 'path_overlay', 'data', 'features'], trailSource.data.features)
+          .set('layers', updatedMapStyle.get('layers').push(trailLayers));
+
+        if (proposedTrailLayers !== undefined) {
+          newMapStyle = newMapStyle
+            .setIn(['sources', 'proposed_overlay'], { type: 'geojson' })
+            .setIn(['sources', 'proposed_overlay', 'data'], { type: 'FeatureCollection' })
+            .setIn(['sources', 'proposed_overlay', 'data', 'features'], proposedTrailSource.data.features)
+            .set('layers', newMapStyle.get('layers').push(proposedTrailLayers));
+        }
+      }
+      return { mapStyle: newMapStyle };
     });
   }
 
-  addLayer(newData, source) {
-    const geoJson = newData.rows.map(rows => ({type: 'Feature', geometry: JSON.parse(rows.the_geom), properties: { fac_type: rows.fac_type, fac_stat: rows.fac_stat }}));
-    const mapStyleWithNewSource = this.state.mapStyle.setIn(['sources', source], { type: 'geojson' })
-                                                     .setIn(['sources', source, 'data'], { type: 'FeatureCollection' })
-                                                     .setIn(['sources', source, 'data', 'features'], geoJson)
-                                                     .set('layers', this.state.mapStyle.get('layers').push(layers.get('layers').find(layer => layer.get('source') === source)))
-    this.setState({
-      mapStyle: mapStyleWithNewSource
-    })
-  }
-
-  componentDidMount(){
-    const mapboxobj = this.mapRef.current.getMap()
-    mapboxobj.addControl(new mapboxgl.ScaleControl({
-      maxWidth: 100,
-      unit: 'imperial'
-    }), 'bottom-right')
-  }
-
   render() {
+    const { mapStyle } = this.state;
     return (
       <div className="test">
         <ReactMapGL
           ref={this.mapRef}
-          width='100vw'
-          height='100vh'
+          width="100vw"
+          height="100vh"
           {...this.state.viewport}
-          onViewportChange={(viewport) => { const {width, height, ...etc} = viewport
-                                            this.setState({viewport: etc}); }}
+          onViewportChange={(viewport) => {
+            const { width, height, ...etc } = viewport;
+            this.setState({ viewport: etc });
+          }}
           mapboxApiAccessToken={process.env.MAPBOX_API_TOKEN}
-          mapStyle={this.state.mapStyle}>
+          mapStyle={mapStyle}
+        >
 
           <div className="zoom-wrapper">
             <NavigationControl />
           </div>
 
           <GeolocateControl
-            positionOptions={{enableHighAccuracy: true}}
-            trackUserLocation={true}
+            positionOptions={{ enableHighAccuracy: true }}
+            trackUserLocation
             className="control-panel__geolocate"
           />
           <ControlPanelToggleButton />
+          <ControlPanel
+            mapStyle={mapStyle}
+            layers={mapStyle.get('layers')}
+            updateStateWith={this.updateStateWith}
+          />
           <Geocoder
             mapRef={this.mapRef}
-            onViewportChange={(viewport) => { const {width, height, ...etc} = viewport
-                                              this.setState({viewport: etc}); }}
+            onViewportChange={(viewport) => {
+              const { width, height, ...etc } = viewport;
+              this.setState({ viewport: etc });
+            }}
             mapboxApiAccessToken={process.env.MAPBOX_API_TOKEN}
             position="top-left"
             placeholder="Search by city or address"
           />
-          <ControlPanel
-            mapStyle={this.state.mapStyle}
-            layers={this.state.mapStyle.get('layers')}
-            updateStateWith={this.updateStateWith}
+          <BasemapButton />
+          <BasemapPanel 
+            changeBasemap={this.changeBasemap}
           />
           <AboutButton />
           <AboutPanel />
