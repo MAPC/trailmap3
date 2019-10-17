a2c6db1e337ccfcc264b8ea6f93992c28b3f1acd
Annabelle Thomas Taylor <ataylor@mapc.org>
2019-10-17 11:19:30 -0400

    Link to database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :repo_url, "git@github.com:mapc/trailmap3.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/master.key", ".env"
+append :linked_files, "config/master.key", ".env", "config/database.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
