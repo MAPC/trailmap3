75ab6d6129526cb1238fa1a079434803b2588d8f
Annabelle Thomas Taylor <ataylor@mapc.org>
2019-10-28 08:49:51 -0400

    Update title header styling for mobile

--- app/javascript/pages/components/header.jsx
+++ app/javascript/pages/components/header.jsx
@@ -13,7 +13,7 @@ export default class Header extends Component {
       <header className="header">
         <img src="https://www.mapc.org/wp-content/themes/mapc/assets/images/mapc-logo.svg" className="header__image" alt="Metropolitan Area Planning Council"/>
         <h1 className="header__title">
-          <span className="header__title-bold">Trailmap: </span>Metro Boston's Regional Walking and Cycling Map
+          <span className="header__title--bold">Trailmap<span className="header__title--mobile-remove">: </span></span><span className="header__title--mobile-remove">Metro Boston's Regional Walking and Cycling Map</span>
         </h1>
       </header>
     );
