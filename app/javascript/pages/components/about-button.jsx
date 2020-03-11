45140fd73335647432661455ce1f7b4a6935e72c
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-03-11 14:25:10 -0400

    Move about-panel outside of mapbox div to allow for text selection
    * Use absolute positioning to move panel out of normal DOM flow; it was otherwise inheriting a `cursor: grab` CSS rule that made it impossible to select text/click into David’s email address
    * Refactor about-button and about-panel into functional components

--- app/javascript/pages/components/about-button.jsx
+++ app/javascript/pages/components/about-button.jsx
@@ -1,30 +1,21 @@
 import React from 'react';
-import {Component} from 'react';
-import { Portal } from 'react-portal';
 
-
-export default class AboutButton extends Component {
-  constructor(props) {
-    super(props);
-    this.el = document.createElement('div');
-  }
-
-  toggleAboutPanel(event) {
+const AboutButton = () => {
+  const toggleAboutPanel = () => {
     const aboutPanel = document.getElementsByClassName('about-panel')[0];
     if (aboutPanel.className === 'about-panel about-panel--hidden') {
       aboutPanel.className = 'about-panel';
     } else {
       aboutPanel.className = 'about-panel about-panel--hidden';
     }
-  }
-
-  render() {
-    return (
-      <Portal node={document && document.getElementsByClassName('mapboxgl-ctrl-top-right')[0]}>
-        <button className="about-button"
-                aria-label="About"
-                onClick={this.toggleAboutPanel.bind(this)} />
-      </Portal>
-    );
-  }
-}
+  };
+  return (
+    <button
+      className="about-button"
+      aria-label="About"
+      onClick={toggleAboutPanel}
+      type="button"
+    />
+  );
+};
+export default AboutButton;
--- app/javascript/pages/components/about-panel.jsx
+++ app/javascript/pages/components/about-panel.jsx
@@ -1,13 +1,7 @@
-import React, { Component } from 'react';
-import { Portal } from 'react-portal';
+import React from 'react';
 
-export default class AboutPanel extends Component {
-  constructor(props) {
-    super(props);
-    this.el = document.createElement('div');
-  }
-
-  toggleText(event) {
+const AboutPanel = () => {
+  const toggleText = (event) => {
     let target;
 
     if (event.target.className.includes('about-panel__heading')) {
@@ -29,96 +23,94 @@ export default class AboutPanel extends Component {
       target.nextSibling.className = 'about-panel__text about-panel__text--hidden';
       target.querySelector('.about-panel__arrow--down').className = 'about-panel__arrow about-panel__arrow--right';
     }
-  }
+  };
 
-  render() {
-    return (
-      <Portal node={document && document.getElementsByClassName('mapboxgl-ctrl-top-right')[0]}>
-        <div className="about-panel about-panel--hidden">
-          <div className="about-panel__object">
-            <button
-              onClick={this.toggleText.bind(this)}
-              className="about-panel__heading about-panel__heading--first"
-              type="button"
-            >
-              <div className="about-panel__arrow about-panel__arrow--right" />
-              About
-            </button>
-            <p className="about-panel__text about-panel__text--hidden">
-              This map is a comprehensive map of pedestrian and bicycle facilities throughout the MAPC region and beyond. The data on this map tool has been collected from a number of sources including city/town trail data, land trusts, DCR, MassDOT, and other sources.
-            </p>
-          </div>
-          <div className="about-panel__object">
-            <button
-              onClick={this.toggleText.bind(this)}
-              className="about-panel__heading"
-              type="button"
-            >
-              <div className="about-panel__arrow about-panel__arrow--right" />
-              Glossary
-            </button>
-            <p className="about-panel__text about-panel__text--hidden">
-              <span className="about-panel__glossary">
-                <strong>Paved Paths </strong>
-                - Hard packed accessible surface, typically asphalt or stonedust
-              </span>
-              <br />
-              <span className="about-panel__glossary">
-                <strong>Unimproved Paths </strong>
-                - Future paved paths, currently with an unimproved natural surface
-              </span>
-              <br />
-              <span className="about-panel__glossary">
-                <strong>Protected Bike Lane </strong>
-                - Physically separated from motor vehicle traffic
-              </span>
-              <br />
-              <span className="about-panel__glossary">
-                <strong>Bike Lane </strong>
-                - Striped lane within the roadway adjacent to traffic
-              </span>
-              <br />
-              <span className="about-panel__glossary">
-                <strong>Paved Footway </strong>
-                - Hard surface path, typically in city park or campus environments
-              </span>
-              <br />
-              <span className="about-panel__glossary">
-                <strong>Natural Surface Footway </strong>
-                - Hiking trail, typically found in conservation areas
-              </span>
-            </p>
-          </div>
-          <div className="about-panel__object">
-            <button
-              onClick={this.toggleText.bind(this)}
-              className="about-panel__heading"
-              type="button"
-            >
-              <div className="about-panel__arrow about-panel__arrow--right" />
-            Disclaimer
-            </button>
-            <p className="about-panel__text about-panel__text--hidden">
-              The data herein is provided for informational purposes only. MAPC makes no warranties, either expressed or implied, and assumes no responsibility for its completeness or accuracy. Users assume all responsibility and risk associated with use of the map and agree to indemnify and hold harmless MAPC with respect to any and all claims and demands that may arise resulting from use of this map.
-            </p>
-          </div>
-          <div className="about-panel__object">
-            <button
-              onClick={this.toggleText.bind(this)}
-              className="about-panel__heading"
-              type="button"
-            >
-              <div className="about-panel__arrow about-panel__arrow--right" />
-              Contribute to this Map
-            </button>
-            <p className="about-panel__text about-panel__text--hidden">
-              We welcome contributions to this map including adding to and editing the data! We have an online editing tool available for you to use. To found out how to access the editing functions, please contact David Loutzenheiser at
-              {' '}
-              <a href="mailto:dloutzenheiser@mapc.org">dloutzenheiser@mapc.org</a>
-            </p>
-          </div>
-        </div>
-      </Portal>
-    );
-  }
-}
+  return (
+    <div className="about-panel about-panel--hidden">
+      <div className="about-panel__object">
+        <button
+          onClick={event => toggleText(event)}
+          className="about-panel__heading"
+          type="button"
+        >
+          <div className="about-panel__arrow about-panel__arrow--right" />
+          About
+        </button>
+        <p className="about-panel__text about-panel__text--hidden">
+          This map is a comprehensive map of pedestrian and bicycle facilities throughout the MAPC region and beyond. The data on this map tool has been collected from a number of sources including city/town trail data, land trusts, DCR, MassDOT, and other sources.
+        </p>
+      </div>
+      <div className="about-panel__object">
+        <button
+          onClick={event => toggleText(event)}
+          className="about-panel__heading"
+          type="button"
+        >
+          <div className="about-panel__arrow about-panel__arrow--right" />
+          Glossary
+        </button>
+        <p className="about-panel__text about-panel__text--hidden">
+          <span className="about-panel__glossary">
+            <strong>Paved Paths </strong>
+            - Hard packed accessible surface, typically asphalt or stonedust
+          </span>
+          <br />
+          <span className="about-panel__glossary">
+            <strong>Unimproved Paths </strong>
+            - Future paved paths, currently with an unimproved natural surface
+          </span>
+          <br />
+          <span className="about-panel__glossary">
+            <strong>Protected Bike Lane </strong>
+            - Physically separated from motor vehicle traffic
+          </span>
+          <br />
+          <span className="about-panel__glossary">
+            <strong>Bike Lane </strong>
+            - Striped lane within the roadway adjacent to traffic
+          </span>
+          <br />
+          <span className="about-panel__glossary">
+            <strong>Paved Footway </strong>
+            - Hard surface path, typically in city park or campus environments
+          </span>
+          <br />
+          <span className="about-panel__glossary">
+            <strong>Natural Surface Footway </strong>
+            - Hiking trail, typically found in conservation areas
+          </span>
+        </p>
+      </div>
+      <div className="about-panel__object">
+        <button
+          onClick={event => toggleText(event)}
+          className="about-panel__heading"
+          type="button"
+        >
+          <div className="about-panel__arrow about-panel__arrow--right" />
+        Disclaimer
+        </button>
+        <p className="about-panel__text about-panel__text--hidden">
+          The data herein is provided for informational purposes only. MAPC makes no warranties, either expressed or implied, and assumes no responsibility for its completeness or accuracy. Users assume all responsibility and risk associated with use of the map and agree to indemnify and hold harmless MAPC with respect to any and all claims and demands that may arise resulting from use of this map.
+        </p>
+      </div>
+      <div className="about-panel__object">
+        <button
+          onClick={event => toggleText(event)}
+          className="about-panel__heading"
+          type="button"
+        >
+          <div className="about-panel__arrow about-panel__arrow--right" />
+          Contribute to this Map
+        </button>
+        <p className="about-panel__text about-panel__text--hidden">
+          We welcome contributions to this map including adding to and editing the data! We have an online editing tool available for you to use. To found out how to access the editing functions, please contact David Loutzenheiser at
+          {' '}
+          <a href="mailto:dloutzenheiser@mapc.org">dloutzenheiser@mapc.org</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPanel;
